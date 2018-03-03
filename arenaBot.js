88e034e56113b892a3d285d878962677d4461fbe
Justin Zauderer <jzauds@gmail.com>
2018-03-02 21:01:41 -0500

    Added ability to challenge users to a duel

--- arenaBot.js
+++ arenaBot.js
@@ -2,13 +2,30 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
-  console.log("I am ready!");
+  console.log("Reactor Online, Sensors Online, Weapons Online, All Systems Nominal");
 });
 
-client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
+client.on("message", function(message) {
+  if (message.content.startsWith(")duel")){
+  	if(!message.mentions.members.size){
+  		message.channel.send("You must tag a user to challenge them");
+  		return;
+  	}
+  	else{
+  		let challenged = message.mentions.members.first();
+		message.channel.send(message.member.displayName + " has challenged " +challenged.toString() + " to combat! If you accept, say any message with the word \"accept\" in it! This offer will expire in 1 minute.");
+		const filter = m => (m.member === challenged && m.content.includes("accept"));
+		message.channel.awaitMessages(filter, {max:1, time:60000})
+		.then(collected => {
+			if(collected.size > 0){
+				message.channel.send("The duel between "+message.member.displayName+" and "+challenged.displayName+" begins!");
+				//beginDuel(message.member, challenged, channel);
+				return;
+			}
+		})
+		.catch("The duel was rejected.")
+  	}
   }
 });
 
-client.login("token");
+client.login("token goes here");
